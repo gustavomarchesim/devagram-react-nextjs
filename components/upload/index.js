5efd2092da3f8c27e81d0676f623e221dfac0351
Gustavo Marchesim <105670464+gustavomarchesim@users.noreply.github.com>
2023-08-22 15:27:47 -0300

    Componente Upload Imagem

--- components/upload/index.js
+++ components/upload/index.js
@@ -1,23 +1,63 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export function UploadImagem({
-    className =''
+  className = "",
+  setImagem,
+  imagemPreview,
+  imagemPreviewClassName = "",
+  aoSetarReferencia,
 }) {
-    
-    const refInput = useRef(null)
+  const refInput = useRef(null);
 
-    const abrirSeletorArquivos = () => {
-        refInput?.current?.click();
+  useEffect(() => {
+    if (!aoSetarReferencia) {
+      return;
     }
 
-    return(
-        <div className={`uploadImagemContainer ${className}`} onClick={abrirSeletorArquivos}>
-            <input
-                type="file" 
-                className="oculto" 
-                accept="image/*"
-                ref={refInput}
-            />
+    aoSetarReferencia(refInput.current);
+  }, [refInput?.current]);
+
+  const abrirSeletorArquivos = () => {
+    refInput?.current?.click();
+  };
+
+  const aoAlterarImagem = () => {
+    if (!refInput?.current?.files.length) {
+      return;
+    }
+
+    const arquivo = refInput?.current?.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(arquivo);
+    fileReader.onloadend = () => {
+      setImagem({
+        preview: fileReader.result,
+        arquivo,
+      });
+    };
+  };
+
+  return (
+    <div
+      className={`uploadImagemContainer ${className}`}
+      onClick={abrirSeletorArquivos}
+    >
+      {imagemPreview && (
+        <div className="imagemPreviewContainer">
+          <img
+            src={imagemPreview}
+            alt="Imagem Preview"
+            className={imagemPreviewClassName}
+          />
         </div>
-    );
+      )}
+      <input
+        type="file"
+        className="oculto"
+        accept="image/*"
+        ref={refInput}
+        onChange={aoAlterarImagem}
+      />
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
+import { useRef, useState } from "react";
+
 import Avatar from "../components/avatar";
 import Button from "../components/button";
-import {UploadImagem} from '../components/upload';
+import { UploadImagem } from "../components/upload";
 
 export default function Home() {
+  const [imagem, setImagem] = useState(null);
+  const refInput = useRef(null); 
+
   return (
     <>
       <h1>Olá, mundo</h1>
-      <UploadImagem />
-      <div style={{width: 200}}>
+      <button onClick={()=> refInput?.current.click()}>Abrir seletor de Arquivos</button>
+      <div style={{ width: 200 }}>
         <Avatar />
-        <Button 
-          text={"Login"} 
-          onClick={() => console.log("Botão clicado!")} />
+        <UploadImagem 
+          setImagem={setImagem}
+          imagemPreview={imagem?.preview}
+          aoSetarReferencia={(ref) => refInput?.current = ref} 
+        />
       </div>
+      <Button 
+        text={"Login"} 
+        onClick={() => console.log("Botão clicado!")} 
+      />
     </>
   );
 }
